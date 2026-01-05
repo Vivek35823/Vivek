8092ec2a894d7cdc431fa9c36a0b82b0eb201f35
Vivekananda Developer <vivek-devotees@example.com>
2026-01-05 23:45:21 +0600

    Add debug logging to index.tsx

--- index.tsx
+++ index.tsx
@@ -4,10 +4,15 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 
+console.log('App starting...');
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
+  console.error("Root element not found!");
   throw new Error("Could not find root element to mount to");
 }
+
+console.log('Root element found, mounting app...');
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
@@ -17,3 +22,5 @@ root.render(
     </HashRouter>
   </React.StrictMode>
 );
+
+console.log('App mounted successfully');
