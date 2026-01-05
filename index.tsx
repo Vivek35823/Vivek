3d0b7e84ea6a71f83013ae706624d61ddce9da62
Vivekananda Developer <vivek-devotees@example.com>
2026-01-06 02:48:59 +0600

    Trigger new deployment to ensure public images are deployed

--- index.tsx
+++ index.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-// Deploy trigger v3
+// Deploy trigger v4 - images fix
 console.log('App starting...');
 
 const rootElement = document.getElementById('root');
